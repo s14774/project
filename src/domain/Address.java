83bdd5679b42c5503a39e2c18f4abb5133b18d61
aurbanow <aurbanow@lab5l-18.pjwstk.edu.pl>
2014-09-27 19:38:49 +0200

    domain model

---
+++ src/domain/Address.java
@@ -0,0 +1,58 @@
+package domain;
+
+public class Address {
+
+	private String country;
+	private String city;
+	private String postalCode;
+	private String street;
+	private String houseNumber;
+	private String localNumber;
+	
+	private Person person;
+	
+	public Person getPerson() {
+		return person;
+	}
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getHouseNumber() {
+		return houseNumber;
+	}
+	public void setHouseNumber(String houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+	public String getLocalNumber() {
+		return localNumber;
+	}
+	public void setLocalNumber(String localNumber) {
+		this.localNumber = localNumber;
+	}
+	
+	
+}
---
+++ src/domain/Person.java
@@ -0,0 +1,50 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+	public Person() {
+		super();
+		this.addresses = new ArrayList<Address>();
+	}
+	
+	private String firstName;
+	private String surname;
+	private String pesel;
+	
+	private User user;
+	private List<Address> addresses;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getPesel() {
+		return pesel;
+	}
+	public void setPesel(String pesel) {
+		this.pesel = pesel;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
+}
