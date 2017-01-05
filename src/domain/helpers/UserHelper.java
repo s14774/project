258d83ec044f93c29715496b08e286da53fbc108
Mateusz Szymborski <s14774@pjwstk.edu.pl>
2017-01-05 12:46:05 +0100

    Wincyj testow

---
+++ src/domain/helpers/UserHelper.java
@@ -0,0 +1,9 @@
+package domain.helpers;
+
+public class UserHelper {
+	public static boolean checkLogin(String name) {
+		//if(name.matches("[A-Z][a-z]+( [A-Z][a-z]+)"))
+			return true;
+		//return false;
+	}
+}
--- src/test/NameHelperTest.java
+++ src/test/NameHelperTest.java
@@ -27,4 +27,54 @@ public class NameHelperTest {
 		boolean isCorrect = NameHelper.check(correctName);
 		assertTrue(isCorrect);
 	}
+	
+	@Test
+	public void checkSecondNameSmallLetter() {
+		String correctName = "Mateusz szymon";
+		boolean isCorrect = NameHelper.check(correctName);
+		assertFalse(isCorrect);
+	}
+	
+	@Test
+	public void checkBigLetterInside() {
+		String correctName = "MateuszSzymon";
+		boolean isCorrect = NameHelper.check(correctName);
+		assertFalse(isCorrect);
+	}
+	
+	@Test
+	public void checkSpaceAtStart() {
+		String correctName = " Mateusz";
+		boolean isCorrect = NameHelper.check(correctName);
+		assertFalse(isCorrect);
+	}
+	
+	@Test
+	public void checkSpaceAtEnd() {
+		String correctName = "Mateusz ";
+		boolean isCorrect = NameHelper.check(correctName);
+		assertFalse(isCorrect);
+	}
+	
+	@Test
+	public void checkSpaceAtEndSecondName() {
+		String correctName = "Mateusz Szymon ";
+		boolean isCorrect = NameHelper.check(correctName);
+		assertFalse(isCorrect);
+	}
+	
+	@Test
+	public void checkDoubleSpaceBetweenNames() {
+		String correctName = "Mateusz  Szymon";
+		boolean isCorrect = NameHelper.check(correctName);
+		assertFalse(isCorrect);
+	}
+	
+	@Test
+	public void checkTripleName() {
+		String correctName = "Mateusz Szymon Marek";
+		boolean isCorrect = NameHelper.check(correctName);
+		assertFalse(isCorrect);
+	}
+	
 }
